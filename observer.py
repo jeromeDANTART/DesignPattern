c5f76452c7e8084bf0804321e888fc849d56d369
Jerome <71261307+jeromeDANTART@users.noreply.github.com>
2022-06-10 13:43:15 +0200

    Update observer.py

--- observer.py
+++ observer.py
@@ -1,91 +1,86 @@
 class Publisher():
-  def initSubcribers(self):
+  def _initSubcribers(self):
     self._subcribers = []
-      
-  def addSubcribers(self,Subcribers):
-    self._subcribers.append(Subcribers)
-    Subcribers.update(self) 
     
-  def removeSubcribers(self,Subcribers):
+  def _notifySubcribers(self):
+    for s in self._subcribers:
+      s._update(self)
+
+  def _addSubcriber(self,subcriber):
+    self._subcribers.append(subcriber)
+      
+  def addSubcriber(self,subcriber):
+    self._subcribers.append(subcriber)
+    subcriber._addPublisher(self)
+    
+  def removeSubcriber(self,subcriber):
     try:
-      self._subcribers.remove(Subcribers)
-      Subcribers.update(None) 
+      self._subcribers.remove(Subcriber)
+      subcriber.removeSubcribers(self)
     except:
       pass
-      
-  def notifySubcribers(self):
-    for i in self._subcribers:
-      i.update(self)
+
+  def nbOfSubcribers(self):
+    return len(self._subcribers)
 
 class Observable(Publisher):
   def __init__(self):
-    self.initSubcribers()
-    self.setVS(5)
+    self._initSubcribers()
+    self.setValue(False)
 
-  def setVS(self,value):
-    self._valeurSurveille=value
-    self.notifySubcribers()
+  def setValue(self,value):
+    if isinstance(value,bool):
+      self._valeurSurveille=value
+      self._notifySubcribers()
 
-  def getVS(self):
+  def getValue(self):
     return self._valeurSurveille
        
 class Subcribers():
-  def update(self,publisher):
-    if publisher==None:
-      del self._valeurSurveille
-    else:
-      self._valeurSurveille = publisher.getVS()
+  def _initPublisher(self):
+    self._publishers=[]
+    self._values=[]
+
+  def _update(self,publisher):
+    try:
+      i=self._publishers.index(publisher)
+      self._values[i] = publisher.getValue()
+    except:
+      pass
+
+  def _addPublisher(self,publisher):
+    self._publishers.append(publisher)
+    self._values.append(None)
+    self._update(publisher)
+
+  def _removePublisher(self,publisher):
+    try:
+      i=self._publishers.index(publisher)
+      self._publishers.remove(publisher)
+      self._values.pop(i)
+    except:
+      pass
+
+  def nbOfPublisher(self):
+    return len(self._publishers)
+
+  def subcripbeTo(self,publisher):
+    publisher.addSubcriber(self)
+
+  def unsubcripbeTo(self,publisher):
+    publisher.removeSubcriber(self)
+
 
 class Observer(Subcribers):
-  def getVS(self):
+  def __init__(self):
+    self._initPublisher()
+    
+  def getValueOf(self,publisher):
     try:
-      return self._valeurSurveille
+      i=self._publishers.index(publisher)
+      return self._values[i]
     except:
       return "aucun abonnement"
 
-
-# init objet 
-#
-Oable1 = Observable()
-Oable2 = Observable()
-Oer1 = Observer()
-Oer2 = Observer()
-Oer3 = Observer()
-# il y a une mise a jour de la valeur observée, pour les observeurs, lors de la souscription
-Oable1.addSubcribers(Oer1)
-Oable2.addSubcribers(Oer2)
-Oable1.addSubcribers(Oer3)
-Oable2.addSubcribers(Oer3)
-print("Observer1 est abonné a Observable1")
-print("Observer2 est abonné a Observable2")
-print("Observer3 est abonné a Observable1 & Observable2")
-print("--------------")
-print(" Valeur par defaut = 5")
-print("--------------")
-print("Observable1 = " , Oable1.getVS())
-print("Observer1 = " , Oer1.getVS())
-print("Observable2 = " , Oable2.getVS())
-print("Observer2 = " ,Oer2.getVS())
-print("Observer3 = " ,Oer3.getVS())
-print("--------------")
-print(" set Observable1 = 2")
-print("--------------")
-Oable1.setVS(2)
-print("Observable1 = " , Oable1.getVS())
-print("Observer1 = " , Oer1.getVS())
-print("Observable2 = " , Oable2.getVS())
-print("Observer2 = " ,Oer2.getVS())
-print("Observer3 = " ,Oer3.getVS())
-print("--------------")
-print(" set Observable2 = 3")
-print(" desabonnement de Observer2 ")
-print("--------------")
-Oable2.setVS(3)
-Oable2.removeSubcribers(Oer2)
-print("Observable1 = " , Oable1.getVS())
-print("Observer1 = " , Oer1.getVS())
-print("Observable2 = " , Oable2.getVS())
-print("Observer2 = " ,Oer2.getVS())
-print("Observer3 = " ,Oer3.getVS())
-print("--------------")
-print(" Observer3 etant abonné a deux Observables, sa valeur sera la valeur la plus recente des Observables auquels il est abonné")
+  def getValue(self):
+    return self._values
